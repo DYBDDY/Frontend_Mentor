2c8f50cfa23d684d20dfd65db1ee339005b7c9b3
DYBDDY <59996972+DYBDDY@users.noreply.github.com>
2024-01-26 17:58:50 +0800

    Update success.js

--- Newsletter-sign-up-with-success-message/success.js
+++ Newsletter-sign-up-with-success-message/success.js
@@ -1,12 +1,1 @@
-window.onload = function() {
-    // 從 URL 中獲取查詢參數
-    let params = new URLSearchParams(window.location.search);
-    let email = params.get('email');
-  
-    // 在頁面上顯示電子郵件
-    let emailElement = document.querySelector('.quote p');
-    if (emailElement) {
-      emailElement.textContent = "A confirmation email has been sent to " + decodeURIComponent(email) + ". Please open it and click the button inside to confirm your subscription.";
-    }
-  }
-  
+
